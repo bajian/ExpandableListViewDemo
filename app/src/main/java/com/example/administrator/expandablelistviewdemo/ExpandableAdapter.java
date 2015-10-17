5b7541f8869c6a810430907b4af993514ed5dbce
bajian <313066164@qq.com>
2015-10-17 16:04:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/expandablelistviewdemo/ExpandableAdapter.java
@@ -0,0 +1,124 @@
+package com.example.administrator.expandablelistviewdemo;
+
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/10/16.
+ */
+public class ExpandableAdapter extends BaseExpandableListAdapter
+{
+    Activity activity;
+    private List<String> groupArray;
+    private List<List<String>> childArray;
+
+
+    public ExpandableAdapter(Activity a)
+    {
+        activity = a;
+
+
+        groupArray = new ArrayList<String>();
+        childArray = new ArrayList<List<String>>();
+
+        groupArray.add("group");
+        groupArray.add("group2");
+
+        List<String> tempArray = new ArrayList<String>();
+        tempArray.add("child1");
+        tempArray.add("child2");
+        tempArray.add("child3");
+
+        for(int index = 0; index <groupArray.size(); ++index)
+        {
+            childArray.add(tempArray);
+        }
+    }
+    @Override
+    public Object getChild(int groupPosition, int childPosition)
+    {
+        return childArray.get(groupPosition).get(childPosition);
+    }
+    public long getChildId(int groupPosition, int childPosition)
+    {
+        return childPosition;
+    }
+    public int getChildrenCount(int groupPosition)
+    {
+        return childArray.get(groupPosition).size();
+    }
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent)
+    {
+        String string = childArray.get(groupPosition).get(childPosition);
+        return getGenericView(string);
+    }
+    // group method stub
+    public Object getGroup(int groupPosition)
+    {
+        return groupArray.get(groupPosition);
+    }
+    public int getGroupCount()
+    {
+        return groupArray.size();
+    }
+    public long getGroupId(int groupPosition)
+    {
+        return groupPosition;
+    }
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent)
+    {
+        String string = groupArray.get(groupPosition);
+        parent.setClickable(false);
+        return getGenericGroupView(string);
+    }
+    // View stub to create Group/Children 's View
+    public TextView getGenericView(String string)
+    {
+        // Layout parameters for the ExpandableListView
+        AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT, 64);
+        TextView text = new TextView(activity);
+        text.setLayoutParams(layoutParams);
+        // Center the text vertically
+        text.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        // Set the text starting position
+        text.setPadding(36, 0, 0, 0);
+        text.setText(string);
+        return text;
+    }
+
+    // View stub to create Group
+    public TextView getGenericGroupView(String string)
+    {
+        // Layout parameters for the ExpandableListView
+        AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT, 64);
+        TextView text = new TextView(activity);
+        text.setLayoutParams(layoutParams);
+        // Center the text vertically
+        text.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
+        // Set the text starting position
+        text.setPadding(36, 0, 0, 0);
+        text.setText(string);
+        return text;
+    }
+
+    public boolean hasStableIds()
+    {
+        return false;
+    }
+    public boolean isChildSelectable(int groupPosition, int childPosition)
+    {
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/expandablelistviewdemo/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.administrator.expandablelistviewdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        ExpandableListView expandableListView = (ExpandableListView)findViewById(R.id.expandableListView);
+        ExpandableAdapter ad = new ExpandableAdapter(this);
+        int groupCount=ad.getGroupCount();
+        expandableListView.setAdapter(ad);
+        for (int i=0;i<groupCount;i++) {
+            expandableListView.expandGroup(i);
+        }
+
+//        设置viewgroup不能点击收缩
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                return true;
+            }
+        });
+//        长按显示选择dialogue
+        expandableListView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+                menu.setHeaderTitle("选择操作");
+                menu.add(0, 0, 0, "重试");
+                menu.add(0, 1, 0, "删除");
+                menu.add(0, 2, 0, "启动");
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
